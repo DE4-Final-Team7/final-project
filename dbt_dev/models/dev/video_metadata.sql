6b65e199f8d2419e89db7ecbf28dd49f759f741c
dmdmjung <kudosjdm@naver.com>
2025-01-08 04:53:44 +0000

    update date format

--- dbt_dev/models/dev/video_metadata.sql
+++ dbt_dev/models/dev/video_metadata.sql
@@ -7,9 +7,9 @@ WITH popular_video_with_category AS
   FROM {{ source('dev', 'popular_video') }} as video
     LEFT JOIN {{ source('dev', 'video_category') }} as category
       ON video.category_id = category.category_id
-  WHERE date_trunc('hour', to_timestamp(video.created_at, 'YYYY-MM-DD HH24:MI:SS')) =
+  WHERE DATE_TRUNC('HOUR', video.created_at) =
     (
-        SELECT date_trunc('hour', max(to_timestamp(created_at, 'YYYY-MM-DD HH24:MI:SS')))
+        SELECT DATE_TRUNC('HOUR', MAX(created_at))
         FROM {{ source('dev', 'popular_video') }}
     )
 )
@@ -23,6 +23,6 @@ SELECT
   like_count,
   view_count,
   published_at,
-  TO_CHAR(CURRENT_TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS') created_at,
-  TO_CHAR(CURRENT_TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS') updated_at
+  DATE_TRUNC('SECOND', CURRENT_TIMESTAMP) created_at,
+  DATE_TRUNC('SECOND', CURRENT_TIMESTAMP) updated_at
 FROM popular_video_with_category
