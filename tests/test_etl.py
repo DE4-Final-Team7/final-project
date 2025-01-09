3ef313cf53e6454381e94d0ab8cd24b4074e2829
dmdmjung <kudosjdm@naver.com>
2025-01-09 08:34:25 +0000

    add etl unittest

---
+++ tests/test_etl.py
@@ -0,0 +1,30 @@
+
+import pytest
+from box import Box
+from src.data_util import get_data_from_api
+
+
+def test_api():
+    config = \
+    {
+        "api":
+        {
+            "video":
+            {
+                "name": "popular_videos",
+                "url": "https://www.googleapis.com/youtube/v3/videos",
+                "params":
+                {
+                    "part": "snippet, statistics",
+                    "chart": "mostPopular",
+                    "maxResults": "10",
+                    "regionCode": "kr"
+                }
+            }
+        }
+    }
+    config = Box()
+    
+    data = get_data_from_api(config.api.video.url, config.api.video.params)
+    assert len(data.get("items")) == config.api.video.params.maxResults
+
