a474768b9fada7994072ae0b1b18a82ad433011e
dmdmjung <kudosjdm@naver.com>
2025-01-09 07:31:19 +0000

    update answer with bracket

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -7,9 +7,9 @@ text_analysis = TextAnalysis()
 
 def test_sentiment():
     summarized_text = text_analysis.summarize_text(["I loved every second of this amazing movie."])
-    assert text_analysis.analyze_sentiment(summarized_text) == "Positive"
+    assert text_analysis.analyze_sentiment(summarized_text) == ["Positive"]
 
 def test_emotion():
     summarized_text = text_analysis.summarize_text(["I loved every second of this amazing movie."])
-    assert text_analysis.analyze_emotion(summarized_text) == "Love"
+    assert text_analysis.analyze_emotion(summarized_text) == ["Love"]
 
